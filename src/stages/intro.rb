12acf8dba5eb2e27aeaeeda08c306939bfc83588
Dirk Breuer <mail@breuer.io>
2015-11-28 21:56:09 +0100

    Added start and exit buttons

--- src/stages/intro.rb
+++ src/stages/intro.rb
@@ -1,13 +1,39 @@
+class Colors
+  SELECTED = [255,255,255,255].freeze
+  INACTIVE = [255,255,255,150].freeze
+end
+
 define_stage :intro do
   curtain_up do
     create_actor :splash_screen
     create_actor :game_title
 
-    create_actor :positioned_label, text: "Start", font_name: "GameBoy.ttf", x: 0, y: 340, width: viewport.width
-    create_actor :positioned_label, text: "Exit", font_name: "GameBoy.ttf", x: 0, y: 380, width: viewport.width, color: [255, 255, 255, 150]
+    start_label = create_actor :positioned_label,
+                               text: "Start", font_name: "GameBoy.ttf", x: 0, y: 340, width: viewport.width, color: Colors::SELECTED
+    exit_label = create_actor :positioned_label,
+                              text: "Exit", font_name: "GameBoy.ttf", x: 0, y: 380, width: viewport.width, color: Colors::INACTIVE
 
-    input_manager.reg :down, KbSpace do
-      fire :next_stage
+    active_label = :start_label
+
+    input_manager.reg :down, KbDown do
+      exit_label.color = Colors::SELECTED
+      start_label.color = Colors::INACTIVE
+      active_label = :exit_label
+    end
+
+    input_manager.reg :down, KbUp do
+      exit_label.color = Colors::INACTIVE
+      start_label.color = Colors::SELECTED
+      active_label = :start_label
+    end
+
+    input_manager.reg :down, KbReturn do
+      case active_label
+      when :start_label
+        fire :next_stage
+      when :exit_label
+        input_manager.window.close
+      end
     end
   end
 
